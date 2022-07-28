f80e12cac6ebde5d80445ee6f89768f558bcf3ca
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-28 17:13:14 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
+const { employees } = data;
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(
+    (elemento) =>
+      elemento.firstName === employeeName || elemento.lastName === employeeName
+  );
 }
 
 module.exports = getEmployeeByName;
