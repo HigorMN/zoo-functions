d4c1ee50fb23d741199f589bf89347fca27be93f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-28 20:57:50 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+const organized = species.sort((a, b) => (a.name > b.name ? 1 : -1));
 function countAnimals(animal) {
-  // seu código aqui
+  if (typeof animal !== 'undefined') {
+    const { specie, sex } = animal;
+    const findSpecie = organized.find((element) => element.name === specie);
+    if (typeof sex !== 'undefined') {
+      const findSex = findSpecie.residents.filter((element) => element.sex === sex);
+      return findSex.length;
+    }
+    return findSpecie.residents.length;
+  }
+  const count = {};
+  organized.forEach((element) => {
+    count[element.name] = element.residents.length;
+  });
+  return count;
 }
 
 module.exports = countAnimals;
