612eb5ad26b9d0a4d940200ff10852550e3d955f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-28 15:51:15 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+function getSpeciesByIds(...ids) {
+  return species.filter((elemento) =>
+    ids.find((parameto) => parameto === elemento.id));
 }
 
 module.exports = getSpeciesByIds;
