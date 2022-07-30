46965041f8a075d2db27346e0ecdf1b9c511bc6f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-29 23:33:33 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const people = employees.find((e) => e.id === id).responsibleFor;
+  const animal = species.find((e) => e.id === people[0]);
+  const animalAge = animal.residents.map((e) => e.age);
+  const maior = Math.max(...animalAge);
+  const animalOld = animal.residents.find((e) => e.age === maior);
+  return Object.values(animalOld);
 }
 
 module.exports = getOldestFromFirstSpecies;
