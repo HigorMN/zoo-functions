1cd1cf26b56da536e8af2149aaf551b12b8e20b5
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-30 23:42:30 -0300

    Requisito 14 salvar

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const noParameter = () => {
+  const objetc = species.reduce((acc, curr) => {
+    if (!acc[curr.location]) {
+      acc[curr.location] = species.filter((e) => curr.location === e.location).map((n) => n.name);
+    }
+    return acc;
+  }, {});
+  return objetc;
+};
+
+const objetcParameter = (options) => {
+  const objetc = species.reduce((acc, curr) => {
+    if (!acc[curr.location]) {
+      const animalsName = species.filter((e) => curr.location === e.location).reduce((a, c) => {
+        a.push({ [c.name]: c.residents.map((n) => n.name) });
+        return a;
+      }, []);
+      acc[curr.location] = animalsName;
+    }
+    return acc;
+  }, {});
+  return objetc;
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (typeof options !== 'undefined') {
+    const { includeNames, sorted } = options;
+    if (includeNames === true) {
+      return objetcParameter();
+    }
+    if (sorted === true) {
+      return true;
+    }
+  }
+  return noParameter();
 }
 
 module.exports = getAnimalMap;
