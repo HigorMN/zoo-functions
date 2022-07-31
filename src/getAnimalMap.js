cc5e02c81c63e68039386df7ef640f7e2d07c697
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-31 13:51:44 -0300

    Requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -5,35 +5,100 @@ const { species } = data;
 const noParameter = () => {
   const objetc = species.reduce((acc, curr) => {
     if (!acc[curr.location]) {
-      acc[curr.location] = species.filter((e) => curr.location === e.location).map((n) => n.name);
+      acc[curr.location] = species
+        .filter((e) => curr.location === e.location)
+        .map((n) => n.name);
     }
     return acc;
   }, {});
   return objetc;
 };
 
-const objetcParameter = (options) => {
-  const objetc = species.reduce((acc, curr) => {
+const objetcParameter = (options) =>
+  species.reduce((acc, curr) => {
     if (!acc[curr.location]) {
-      const animalsName = species.filter((e) => curr.location === e.location).reduce((a, c) => {
-        a.push({ [c.name]: c.residents.map((n) => n.name) });
-        return a;
-      }, []);
+      const animalsName = species
+        .filter((e) => curr.location === e.location)
+        .reduce((a, c) => {
+          a.push({ [c.name]: c.residents.map((n) => n.name) });
+          return a;
+        }, []);
       acc[curr.location] = animalsName;
     }
     return acc;
   }, {});
-  return objetc;
+
+const objetcParameterSorted = (options) => {
+  const { sorted, includeNames } = options;
+  if (includeNames === true && sorted === true) {
+    return species.reduce((acc, curr) => {
+      if (!acc[curr.location]) {
+        const animalsName = species
+          .filter((e) => curr.location === e.location)
+          .reduce((a, c) => {
+            a.push({ [c.name]: c.residents.map((n) => n.name).sort() });
+            return a;
+          }, []);
+        acc[curr.location] = animalsName;
+      }
+      return acc;
+    }, {});
+  }
+  return objetcParameter();
+};
+
+const objetcFemale = () =>
+  species.reduce((acc, curr) => {
+    if (!acc[curr.location]) {
+      const animalsName = species
+        .filter((e) => curr.location === e.location)
+        .reduce((a, c) => {
+          a.push({
+            [c.name]: c.residents
+              .filter((n) => n.sex === 'female')
+              .map((na) => na.name),
+          });
+          return a;
+        }, []);
+      acc[curr.location] = animalsName;
+    }
+    return acc;
+  }, {});
+const objetcFemaleSorted = () =>
+  species.reduce((acc, curr) => {
+    if (!acc[curr.location]) {
+      const animalsName = species
+        .filter((e) => curr.location === e.location)
+        .reduce((a, c) => {
+          a.push({
+            [c.name]: c.residents
+              .filter((n) => n.sex === 'female')
+              .map((na) => na.name)
+              .sort(),
+          });
+          return a;
+        }, []);
+      acc[curr.location] = animalsName;
+    }
+    return acc;
+  }, {});
+
+const objetcSex = (options) => {
+  const { sex, includeNames, sorted } = options;
+  if (sex === 'female' && includeNames === true) {
+    if (sorted === true) {
+      return objetcFemaleSorted();
+    }
+    return objetcFemale();
+  }
+  return objetcParameterSorted(options);
 };
 
 function getAnimalMap(options) {
   if (typeof options !== 'undefined') {
-    const { includeNames, sorted } = options;
+    const { includeNames } = options;
     if (includeNames === true) {
-      return objetcParameter();
-    }
-    if (sorted === true) {
-      return true;
+      return objetcSex(options);
     }
   }
   return noParameter();
